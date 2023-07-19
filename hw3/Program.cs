61a8052230e140a91f01a9a0e69db65f3d3b54a6
Anastasia Polivanova <lyaminana@gmail.com>
2023-07-19 12:35:59 +0300

    добавила задачу 52

---
+++ hw3/Program.cs
@@ -0,0 +1,58 @@
+﻿//Задача 52. Задайте двумерный массив из целых чисел.
+ //Найдите среднее арифметическое элементов в каждом столбце.
+
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] CreateMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m,n];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(min, max+1);
+        }
+    }
+    return matrix;
+}
+
+int[,] NewMatrix = CreateMatrix(5, 7, 1, 10);
+
+void PrintMatrix(int[,] matrix1)
+{
+    for(int i = 0; i < matrix1.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix1.GetLength(1); j++)
+        {
+            Console.Write(matrix1[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(NewMatrix);
+
+    for(int i = 0; i < NewMatrix.GetLength(0); i++)
+    {
+        double rowsSum = 0;
+        for(int j = 0; j < NewMatrix.GetLength(1); j++)
+        {
+            rowsSum = rowsSum  + NewMatrix[i,j];
+        }
+        Console.WriteLine($"Сумма элементов в строчке {i} = {Math.Round(rowsSum / NewMatrix.GetLength(1),0)}");
+    }
+
+
+for(int i = 0; i < NewMatrix.GetLength(1); i++)
+    {
+        double columnsSum = 0;
+        for(int j = 0; j < NewMatrix.GetLength(0); j++)
+        {
+            columnsSum = columnsSum  + NewMatrix[j,i];
+        }
+        Console.WriteLine($"Сумма элементов в столбце {i} = {Math.Round(columnsSum / NewMatrix.GetLength(0),0)}");
+    }
