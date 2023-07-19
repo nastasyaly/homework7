fe1196877c1b59c22ce8f069caf9e7137fc52a28
Anastasia Polivanova <lyaminana@gmail.com>
2023-07-19 12:35:47 +0300

    добавила задачу 50

---
+++ hw2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 50.
+int[,] GetMatrix(int rows,int columns,int min,int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+            for(int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i,j] = new Random().Next(min,max+1);
+            }
+
+    }
+    return matrix;
+}
+int rows = new Random().Next(1,10);
+int columns = new Random().Next(1,10);
+int[,] Matrix1 = GetMatrix(rows, columns, 1, 100);
+
+void PrintMatrix(int[,] matrix1)
+{
+    for(int i = 0; i < matrix1.GetLength(0); i++)
+    {
+            for(int j = 0; j < matrix1.GetLength(1); j++)
+            {
+                Console.Write(matrix1[i,j] + "\t");
+            }
+         Console.WriteLine();
+    }
+}
+
+PrintMatrix(Matrix1);
+
+Console.Write("Введите первую позицию элемента от 0 до 100 ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второую позицию элемента от 0 до 100 ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a >= rows | b >= columns)
+{
+     Console.WriteLine($"Числа с позициями [{a} , {b}] в массиве нет ");
+}
+else
+{
+     Console.WriteLine($"Вы ввели число из массива");
+
+}
+        
+    
+
